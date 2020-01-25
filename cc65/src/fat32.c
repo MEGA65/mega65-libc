6cb46b99c99c16041cde421f653b276c7f93c2be
Paul Gardner-Stephen <paul@servalproject.org>
2020-01-25 21:53:01 +1030

    fix compilation errors.

--- cc65/src/fat32.c
+++ cc65/src/fat32.c
@@ -28,61 +28,79 @@ long mega65_fat32_create_contiguous_file(char *name, long size,
   unsigned long start_cluster=0;
   unsigned long next_cluster;
   unsigned long contiguous_clusters=0;
-  char message[40]="Found file: ????????.???";
+  unsigned int fat_offset=0;
+  int j;
 
   clusters=size/4096;
+  if (size&4095) clusters++;
 
-  sdcard_readsector(fat1_sector);
-  for(offset=0;offset<512;offset+=4)
-    {
-      next_cluster=sector_buffer[offset];
-      next_cluster|=((long)sector_buffer[offset+1]<<8L);
-      next_cluster|=((long)sector_buffer[offset+2]<<16L);
-      next_cluster|=((long)sector_buffer[offset+3]<<24L);
-      if (!next_cluster) {
-	if (!start_cluster) {
-	  start_cluster=offset/4;
-	}
-	contiguous_clusters++;
-	if (contiguous_clusters==clusters) {
-	  // End of chain marker
-	  sector_buffer[offset+0]=0xff; sector_buffer[offset+1]=0xff;
-	  sector_buffer[offset+2]=0xff; sector_buffer[offset+3]=0x0f;
-	  break;
+  for(fat_offset=0;fat_offset <= (fat2_sector-fat1_sector); fat_offset++) {
+    mega65_sdcard_readsector(fat1_sector+fat_offset);
+    contiguous_clusters=0;
+    start_cluster=0;
+
+    // Skip any FAT sectors with allocated clusters
+    for(j=0;j<512;j++) if (sector_buffer[j]) break;
+    if (j!=512) {
+      continue;
+    }
+    
+    for(offset=0;offset<512;offset+=4)
+      {
+	next_cluster=sector_buffer[offset];
+	next_cluster|=((long)sector_buffer[offset+1]<<8L);
+	next_cluster|=((long)sector_buffer[offset+2]<<16L);
+	next_cluster|=((long)sector_buffer[offset+3]<<24L);
+	if (!next_cluster) {
+	  if (!start_cluster) {
+	    start_cluster=(offset/4)+fat_offset*(512/4);
+	  }
+	  contiguous_clusters++;
+	  if (contiguous_clusters==clusters) {
+	    // End of chain marker
+	    sector_buffer[offset+0]=0xff; sector_buffer[offset+1]=0xff;
+	    sector_buffer[offset+2]=0xff; sector_buffer[offset+3]=0x0f;
+	    break;
+	  } else {
+	    // Point to next cluster
+	    uint32_t the_cluster=(fat_offset*(512/4)+(offset/4))+1;
+	    sector_buffer[offset+0]=(the_cluster>>0)&0xff;
+	    sector_buffer[offset+1]=(the_cluster>>8)&0xff;
+	    sector_buffer[offset+2]=(the_cluster>>16)&0xff;
+	    sector_buffer[offset+3]=(the_cluster>>24)&0xff;
+	  }
 	} else {
-	  // Point to next cluster
-	  sector_buffer[offset+0]=(offset/4)+1;
-	  sector_buffer[offset+1]=0; sector_buffer[offset+2]=0; sector_buffer[offset+3]=0;	  
-	}
-      } else {
-	if (start_cluster) {
-	  // write_line("ERROR: Disk space is fragmented. File not created.",0);
-	  return -1;
+	  if (start_cluster) {
+	    // write_line("ERROR: Disk space is fragmented. File not created.",0);
+	    // 	    return 0;
+	    // Not enough contiguous space in this FAT sector, so try the next
+	    break;
+	  }
 	}
       }
+
+    if (start_cluster&&(contiguous_clusters==clusters)) break;
+    else {
     }
+  }
   if ((!start_cluster)||(contiguous_clusters!=clusters)) {
-    //    write_line("ERROR: Could not find enough free clusters early in file system",0);
+    //    write_line("ERROR: Could not find enough free clusters in file system",0);
     return -1;
   }
-  // write_line("First free cluster is ",0);
-  // screen_decimal(screen_line_address-80+22,start_cluster);
 
   // Commit sector to disk (in both copies of FAT)
-  sdcard_writesector(fat1_sector,0);
-  sdcard_writesector(fat2_sector,0);
+  mega65_sdcard_writesector(fat1_sector+fat_offset);
+  mega65_sdcard_writesector(fat2_sector+fat_offset);
   
-  sdcard_readsector(root_dir_sector);
+  mega65_sdcard_readsector(root_dir_sector);
   
   for(offset=0;offset<512;offset+=32)
     {
-      for(i=0;i<8;i++) message[12+i]=sector_buffer[offset + i];
-      for(i=0;i<3;i++) message[21+i]=sector_buffer[offset + 8 + i];
-      if (message[12]>' ') write_line(message,0);
+      if (sector_buffer[offset]>' ') continue;
       else break;
     }
   if (offset==512) {
-    // write_line("ERROR: First sector of root directory already full.",0);
+    //    write_line("ERROR: First sector of root directory already full.",0);
     return -1;
   }
 
@@ -99,7 +117,8 @@ long mega65_fat32_create_contiguous_file(char *name, long size,
   sector_buffer[offset+0x1E]=(size>>16L)&0xff; 
   sector_buffer[offset+0x1F]=(size>>24l)&0xff;
 
-  sdcard_writesector(root_dir_sector,0);
-  
+  mega65_sdcard_writesector(root_dir_sector);
+
   return root_dir_sector+(start_cluster-2)*8;
 }
+
