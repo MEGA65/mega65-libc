68acf4274b37f8ec793f799946dff125e53a07c4
Paul Gardner-Stephen <paul@servalproject.org>
2020-02-20 12:13:10 +1030

    implement target detection.

--- cc65/src/targets.c
+++ cc65/src/targets.c
@@ -3,6 +3,15 @@
 
 unsigned char detect_target(void)
 {
-
+  // We use the different I2C device blocks to identify the various hardware targets
   
+  if (lpeek(0xffd7000)||lpeek(0xffd7001)||
+      lpeek(0xffd7002)||lpeek(0xffd7004))
+    return TARGET_MEGAPHONER1;
+
+  if (lpeek(0xffd7100)||lpeek(0xffd7101)||
+      lpeek(0xffd7102)||lpeek(0xffd7104))
+    return TARGET_MEGA65R2;
+
+  return TARGET_UNKNOWN;
 }
