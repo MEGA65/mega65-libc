ed12b949cf60af6d16a8cd56cc336c62f3c4518b
Paul Gardner-Stephen <paul@servalproject.org>
2020-07-01 16:44:13 +0930

    add mouse routines

---
+++ cc65/include/mouse.h
@@ -0,0 +1,13 @@
+void mouse_warp_to(unsigned short x,unsigned short y);
+void mouse_update_position(unsigned short *mx,unsigned short *my);
+void mouse_update_pointer(void);
+char mouse_clicked(void);
+void mouse_clip_position(void);
+void mouse_bind_to_sprite(unsigned char sprite_num);
+void mouse_set_bounding_box(unsigned short x1, unsigned short y1,unsigned short x2,unsigned short y2);
+
+
+
+
+
+
