3c4c84f71b5f3ca4e706066da9754ba6bb57db98
Paul Gardner-Stephen <paul@servalproject.org>
2020-07-05 17:48:40 +0930

    fix debug_msg()

--- cc65/src/debug.c
+++ cc65/src/debug.c
@@ -1,12 +1,14 @@
 #include "memory.h"
 
+unsigned char the_char;
 void debug_msg(char *m)
 {
   // Write debug message to serial monitor
-  while(*m) {    
-    asm ("LDA %v",*m);
-    asm ("STA $D643");
-    asm ("NOP");
+  while(*m) {
+    the_char=*m;
+    __asm__ ("LDA %v",the_char);
+    __asm__ ("STA $D643");
+    __asm__ ("NOP");
     m++;
   }
 }
