ec901b25e3b7550d21b389aff76878dd529ffd3d
Oliver Graf <ograf@devilsclaw.de>
2022-10-08 13:46:53 +0200

    fix: MEGAphone R4 hwids for clang and kickc fixed

--- kickc/include/targets.h
+++ kickc/include/targets.h
@@ -4,7 +4,7 @@
 #define TARGET_UNKNOWN 0
 #define TARGET_MEGA65R2 1
 #define TARGET_MEGAPHONER1 0x21
-#define TARGET_MEGAPHONER1 0x22
+#define TARGET_MEGAPHONER4 0x22
 
 unsigned char detect_target(void);
 
