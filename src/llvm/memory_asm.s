93a22575ab79273cafc0be050bc9bb8960ae3f70
Mikael Lund <Mlund@mac.com>
2023-07-19 14:46:04 +0200

    Use explicit registers
    Zero-page registers are assumed to be continuous.

--- src/llvm/memory_asm.s
+++ src/llvm/memory_asm.s
@@ -1,13 +1,13 @@
-.global lpeek, lpoke
-.section code,"a"
+.global lpoke, lpeek
+.section code, "a"
 lpoke:
-        ; copy 32-bit input address (a, x, rc2, rc3) to rc5
+        ; copy 32-bit input address (a, x, rc2-rc3) to rc5-rc8
         sta __rc5
-        stx __rc5 + 1
+        stx __rc6
         lda __rc2
-        sta __rc5 + 2
+        sta __rc7
         lda __rc3
-        sta __rc5 + 3
+        sta __rc8
         ; 8-bit input value (rc4)
         lda __rc4
         ldz #0
@@ -16,13 +16,13 @@ lpoke:
         ldz #0
         rts
 lpeek:
-        ; copy 32-bit input address (a, x, rc2, rc3) to rc4
+        ; copy 32-bit input address (a, x, rc2-rc3) to rc4-rc7
         sta __rc4
-        stx __rc4 + 1
+        stx __rc5
         lda __rc2
-        sta __rc4 + 2
+        sta __rc6
         lda __rc3
-        sta __rc4 + 3
+        sta __rc7
         ldz #0
         ldx #0
         nop
