b22e13973e8e3d81f65f031c90e395942c8274d3
Mikael Lund <Mlund@mac.com>
2023-07-21 11:09:00 +0200

    Packed attribute for m65_dirent

--- include/mega65/dirent.h
+++ include/mega65/dirent.h
@@ -7,7 +7,11 @@ unsigned char opendir(void);
 struct m65_dirent* readdir(unsigned char);
 void closedir(unsigned char);
 
+#ifdef __clang__
+struct __attribute__((__packed__)) m65_dirent {
+#else
 struct m65_dirent {
+#endif
     uint32_t d_ino;
     uint16_t d_off;
     uint32_t d_reclen;
