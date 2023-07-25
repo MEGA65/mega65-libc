778d05af08d4dcf09e7dd31353616daaf1955a87
Mikael Lund <Mlund@mac.com>
2023-07-25 17:23:30 +0200

    Dirent doxygen comments

--- include/mega65/dirent.h
+++ include/mega65/dirent.h
@@ -3,20 +3,26 @@
 
 #include <stdint.h>
 
+/// Open a directory
 unsigned char opendir(void);
+
+/// Read directory entry
 struct m65_dirent* readdir(unsigned char);
+
+/// Close directory entry
 void closedir(unsigned char);
 
+/// Structure describing an open directory
 #ifdef __clang__
 struct __attribute__((__packed__)) m65_dirent {
 #else
 struct m65_dirent {
 #endif
-    uint32_t d_ino;
-    uint16_t d_off;
-    uint32_t d_reclen;
-    uint16_t d_type;
-    char d_name[256];
+    uint32_t d_ino;    //!< File number of entry
+    uint16_t d_off;    //!< Offset
+    uint32_t d_reclen; //!< Length of this record
+    uint16_t d_type;   //!< File type
+    char d_name[256];  //!< Filename string of entry
 };
 
 #endif // __MEGA65_DIRENT_H
