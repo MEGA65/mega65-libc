4cb7b29af92d9be660b34a1111da47b1c1556203
Mikael Lund <Mlund@mac.com>
2023-08-02 19:05:00 +0200

    Refactor llvm fileio.s

--- src/llvm/fileio.s
+++ src/llvm/fileio.s
@@ -1,137 +1,124 @@
-    .global closeall, open, close, read512, toggle_rom_write_protect, chdir, chdirroot
+.global closeall, open, close, read512, toggle_rom_write_protect, chdir, chdirroot
 
-    ;; allocate space in zeropage
-    .section .zeropage,"aw",@nobits
+HYPPO_GETVERSION = $00;
+HYPPO_CHDIR = $0C;
+HYPPO_OPENFILE = $18;
+HYPPO_READFILE = $1A;
+HYPPO_CLOSEFILE = $20;
+HYPPO_CLOSEALL = $22;
+HYPPO_SETNAME = $2E;
+HYPPO_FINDFILE = $34;
+HYPPO_TOGGLE_ROM_WRITE_PROTECT = $70;
+FILE_ERROR = $FF;
+NAME_ERROR = $FF;
+
+.section .zeropage,"aw",@nobits
 ptr1:
-        .ds.b 2
+	.ds.b 2
 
-    .section code,"a"
-
-mega65_io_enable:
-	lda #$47
-	sta $d02f
-	lda #$53
-	sta $d02f
-	rts
-        
+.section code,"a"
+    
 cc65_copy_ptr1_string_to_0100:	
-        ;; Copy file name
+	; copy file name
 	phy
-        ldy #0
+	ldy #0
 NameCopyLoop:
-        lda (ptr1),y
-        sta $0100,y
-        iny
-        cmp #0
-        bne NameCopyLoop
+	lda (ptr1),y
+	sta $0100,y
+	iny
+	cmp #0
+	bne NameCopyLoop
 	ply
 	rts	
 
 setname_0100:
-	;;  Call dos_setname()
+	; call dos_setname()
 	ldy #>$0100
 	ldx #<$0100
-	jsr mega65_io_enable
-	lda #$2E                ; dos_setname Hypervisor trap
+	lda #HYPPO_SETNAME
 	sta $D640               ; Do hypervisor trap
 	clv                     ; Wasted instruction slot required following hyper trap instruction
-	;; XXX Check for error (carry would be clear)
+	; XXX Check for error (carry would be clear)
 	bcs setname_ok
-	lda #$ff
-	rts
+	lda #NAME_ERROR
 setname_ok:
 	rts
 	
-	;; closeall
 closeall:
-	jsr mega65_io_enable
-	LDA #$22
-	STA $D640
-	CLV
-	;Not needed on llvm-mos
-	;LDX #$00
-	RTS
+	lda #HYPPO_CLOSEALL
+	sta $D640
+	clv
+	rts
 
 toggle_rom_write_protect:
-	jsr mega65_io_enable
-	lda #$70
-	STA $d640
+	lda #HYPPO_TOGGLE_ROM_WRITE_PROTECT
+	sta $D640
 	clv
-	;ldx #0
 	rts
 	
 read512:
-	;;  Get pointer to buffer
+	;  Get pointer to buffer
 	lda __rc2
 	ldx __rc3
 	sta ptr1+0
 	stx ptr1+1
-
-	;; Select current file
-	;; XXX - Not currently implemented
-	
-	;; Read the next sector of data
-	jsr mega65_io_enable
-	LDA #$1A
-	STA $D640
+	; Select current file
+	; XXX - Not currently implemented
+	; Read the next sector of data
+	lda #HYPPO_READFILE
+	sta $D640
 	clv
-	;LDX #$00
-
-	;; Number of bytes read returned in X and Y
-	;; Store these for returning
+	; bytes read returned in X and Y; stash these for returning
 	stx __rc2
 	sty __rc3
 
-	;; Make sure SD buffer is selected, not FDC buffer
+	; Make sure SD buffer is selected, not FDC buffer
 	lda #$80
 	tsb $D689
 	
-	;; Copy the full 512 bytes from the sector buffer at $FFD6E00
-	;; (This saves the need to mess with mapping/unmapping the sector
-	;; buffer).
-
-	;; Get address to save to
+	; Copy the full 512 bytes from the sector buffer at $FFD6E00
+	; (This saves the need to mess with mapping/unmapping the sector
+	; buffer).
+	; Get address to save to
 	lda ptr1+0
 	sta copysectorbuffer_destaddr+0
 	lda ptr1+1
 	sta copysectorbuffer_destaddr+1
 
-	;; Execute DMA job
+	; do DMA job
 	lda #$00
-	sta $d702
-	sta $d704
+	sta $D702
+	sta $D704
 	lda #>dmalist_copysectorbuffer
-	sta $d701
+	sta $D701
 	lda #<dmalist_copysectorbuffer
-	sta $d705
+	sta $D705
 
-	;; Retrieve the return value
+	; Return file length word through A and X
 	lda __rc2
 	ldx __rc3
-	RTS	
+	rts
 
-	.data
-
+.data
 dmalist_copysectorbuffer:
-	;; Copy $FFD6E00 - $FFD6FFF down to low memory 
-	;; MEGA65 Enhanced DMA options
-        .byte $0A  ;; Request format is F018A
-        .byte $80,$FF ;; Source is $FFxxxxx
-        .byte $81,$00 ;; Destination is $FF
-        .byte $00  ;; No more options
-        ;; F018A DMA list
-        ;; (MB offsets get set in routine)
-        .byte $00 ;; copy + last request in chain
-        .short $0200 ;; size of copy is 512 bytes
-        .short $6E00 ;; starting at $6E00
-        .byte $0D   ;; of bank $D
+	; Copy $FFD6E00 - $FFD6FFF down to low memory 
+	; MEGA65 Enhanced DMA options
+	.byte $0A  ;; Request format is F018A
+	.byte $80,$FF ;; Source is $FFxxxxx
+	.byte $81,$00 ;; Destination is $FF
+	.byte $00  ;; No more options
+	; F018A DMA list
+	; (MB offsets get set in routine)
+	.byte $00 ;; copy + last request in chain
+	.short $0200 ;; size of copy is 512 bytes
+	.short $6E00 ;; starting at $6E00
+	.byte $0D   ;; of bank $D
 copysectorbuffer_destaddr:	
-        .short $8000 ;; destination address is $8000
-        .byte $00   ;; of bank $0
-        .short $0000 ;; modulo (unused)
+	.short $0000 ;; destination address
+	.byte $0   ;; of bank $0
+	.short $0000 ;; modulo (unused)
 
-    .section code,"a"
-
+.section code,"a"
 open:
     ; argument pointer stored in rc2 and rc3
 	lda __rc2
@@ -142,55 +129,39 @@ open:
 	jsr cc65_copy_ptr1_string_to_0100
 	jsr setname_0100	
 
-	;; Find file
-	; Look for file on FAT file system via hypervisor calls
-	lda #$34
-	sta $d640
+	lda #HYPPO_FINDFILE
+	sta $D640
 	clv
 	bcs open_file_exists
-
-	;; No such file.
-	lda #$ff
-	;tax
+	lda #FILE_ERROR
 	rts
 
 open_file_exists:	
-	jsr mega65_io_enable
-	; hyppo_getversion (outputs to A, X, Y, Z)
-	lda #$00
-	sta $d640
-	clv
-	
-	; hyppo_openfile (outputs to A)
-	lda #$18
+	lda #HYPPO_GETVERSION; outputs to A, X, Y, Z
 	sta $D640
 	clv
-
-	; make sure Z is cleared before exiting
-	ldz #$00
+	lda #HYPPO_OPENFILE; outputs to A
+	sta $D640
+	clv
+	ldz #$00; Z must be cleared before exiting
 	rts
 
 close:
-	TAX
-	jsr mega65_io_enable
-	LDA #$20
-	STA $D640
+	tax
+	lda #HYPPO_CLOSEFILE; input in X
+	sta $D640
 	clv
-	;LDX #$00
-	RTS
+	rts
 
 chdirroot:
-	;; Change to root directory of volume
-
-	lda #$3C
-	sta $d640
+	lda #$3c; this seems undocumented as of July 2023?
+	sta $D640
 	clv
-
 	ldx #$00
 	rts
 	
 chdir:
-        ;; Get pointer to file name
+    ; Get pointer to file name
 	lda __rc2
 	ldx __rc3
 	sta ptr1+0
@@ -199,31 +170,17 @@ chdir:
 	jsr cc65_copy_ptr1_string_to_0100
 	jsr setname_0100	
 
-	; hyppo_findfile
-	; Look for file on FAT file system via hypervisor calls
-	lda #$34
-	sta $d640
+	lda #HYPPO_FINDFILE
+	sta $D640
 	clv
 	bcs chdir_file_exists
-
-	;; No such file.
-	lda #$ff
-	tax
+	lda #FILE_ERROR
 	rts
-
 chdir_file_exists:	
-	;; Actually call chdir
-	jsr mega65_io_enable
-	; hyppo_chdir
-	lda #$0C
-	sta $d640
+	lda #HYPPO_CHDIR
+	sta $D640
 	clv
-	
-	; hyppo_openfile (outputs to A)
-	LDA #$18
-	STA $D640
+	lda #HYPPO_OPENFILE; outputs to A
+	sta $D640
 	clv
-	
-	;; Not needed on llvm-mos
-	;;LDX #$00
-	RTS
+	rts
