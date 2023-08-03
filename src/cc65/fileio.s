a8a1412245ca9464da1d703f8dd604f27afd9ba9
Mikael Lund <Mlund@mac.com>
2023-08-03 15:12:02 +0200

    Reformat cc65 dirent.s and fileio.s

--- src/cc65/fileio.s
+++ src/cc65/fileio.s
@@ -1,11 +1,8 @@
-
 	.setcpu "65C02"
 	.export _closeall, _open, _close, _read512, _toggle_rom_write_protect, _chdir, _chdirroot
-
 	.include "zeropage.inc"
 	
 .SEGMENT "CODE"
-
 	.p4510
 
 mega65_io_enable:
@@ -16,80 +13,71 @@ mega65_io_enable:
 	rts
 	
 cc65_copy_ptr1_string_to_0100:	
-        ;; Copy file name
+    ;; Copy file name
 	phy
-        ldy #0
+	ldy #0
 @NameCopyLoop:
-        lda (ptr1),y
-        sta $0100,y
-        iny
-        cmp #0
-        bne @NameCopyLoop
+	lda (ptr1),y
+	sta $0100,y
+	iny
+	cmp #0
+	bne @NameCopyLoop
 	ply
 	rts	
 
 setname_0100:
-        ;;  Call dos_setname()
-        ldy #>$0100
-        ldx #<$0100
+	;;  Call dos_setname()
+	ldy #>$0100
+	ldx #<$0100
 	jsr mega65_io_enable
-        lda #$2E                ; dos_setname Hypervisor trap
-        STA $D640               ; Do hypervisor trap
-        NOP                     ; Wasted instruction slot required following hyper trap instruction
-        ;; XXX Check for error (carry would be clear)
+	lda #$2E                ; dos_setname Hypervisor trap
+	sta $D640               ; Do hypervisor trap
+	clv                     ; Wasted instruction slot required following hyper trap instruction
+	;; XXX Check for error (carry would be clear)
 	bcs setname_ok
 	lda #$ff
 	rts
 setname_ok:
 	RTS
-
 	
-	;; closeall
 _closeall:
 	jsr mega65_io_enable
-	LDA #$22
-	STA $D640
-	NOP
-	LDX #$00
-	RTS
+	lda #$22
+	sta $D640
+	clv
+	ldx #$00
+	rts
 
 _toggle_rom_write_protect:
 	jsr mega65_io_enable
 	lda #$70
-	STA $d640
-	nop
+	sta $d640
+	clv
 	ldx #0
 	rts
 	
 _read512:
-
 	;;  Get pointer to buffer
 	sta ptr1+0
 	stx ptr1+1
-
 	;; Select current file
 	;; XXX - Not currently implemented
-	
 	;; Read the next sector of data
 	jsr mega65_io_enable
-	LDA #$1A
-	STA $D640
-	NOP
-	LDX #$00
-
+	lda #$1A
+	sta $D640
+	clv
+	ldx #$00
 	;; Number of bytes read returned in X and Y
 	;; Store these for returning
 	stx tmp2
 	sty tmp3
-
 	;; Make sure SD buffer is selected, not FDC buffer
 	lda #$80
 	tsb $D689
-	
 	;; Copy the full 512 bytes from the sector buffer at $FFD6E00
 	;; (This saves the need to mess with mapping/unmapping the sector
 	;; buffer).
-
 	;; Get address to save to
 	lda ptr1+0
 	sta copysectorbuffer_destaddr+0
@@ -108,10 +96,9 @@ _read512:
 	;; Retrieve the return value
 	lda tmp2
 	ldx tmp3
-	RTS	
+	rts	
 
 	.data
-
 dmalist_copysectorbuffer:
 	;; Copy $FFD6E00 - $FFD6FFF down to low memory 
 	;; MEGA65 Enhanced DMA options
@@ -130,12 +117,9 @@ copysectorbuffer_destaddr:
         .byte $00   ;; of bank $0
         .word $0000 ;; modulo (unused)
 
-	
-	.code
-	
+	.code	
 _open:
-
-        ;; Get pointer to file name
+	;; Get pointer to file name
 	sta ptr1+0
 	stx ptr1+1
 	
@@ -148,7 +132,6 @@ _open:
 	sta $d640
 	nop
 	bcs open_file_exists
-
 	;; No such file.
 	lda #$ff
 	tax
@@ -159,40 +142,34 @@ open_file_exists:
 	jsr mega65_io_enable
 	lda #$00
 	sta $d640
-	nop
-	
-	LDA #$18
-	STA $D640
-	NOP
-	
-	LDX #$00
-	RTS
+	clv	
+	lda #$18
+	sta $D640
+	clv
+	ldx #$00
+	rts
 
 _close:
-	TAX
+	tax
 	jsr mega65_io_enable
-	LDA #$20
-	STA $D640
-	NOP
-	LDX #$00
-	RTS
+	lda #$20
+	sta $D640
+	clv
+	ldx #$00
+	rts
 
 _chdirroot:
 	;; Change to root directory of volume
-
 	lda #$3C
 	sta $d640
-	nop
-
+	clv
 	ldx #$00
 	rts
-	
-_chdir:
 
-        ;; Get pointer to file name
+_chdir:
+	;; Get pointer to file name
 	sta ptr1+0
-	stx ptr1+1
-	
+	stx ptr1+1	
 	jsr cc65_copy_ptr1_string_to_0100
 	jsr setname_0100	
 
@@ -200,7 +177,7 @@ _chdir:
 	; Look for file on FAT file system via hypervisor calls
 	lda #$34
 	sta $d640
-	nop
+	clv
 	bcs chdir_file_exists
 
 	;; No such file.
@@ -213,11 +190,9 @@ chdir_file_exists:
 	jsr mega65_io_enable
 	lda #$0C
 	sta $d640
-	nop
-	
-	LDA #$18
-	STA $D640
-	NOP
-	
-	LDX #$00
-	RTS
+	clv
+	lda #$18
+	sta $D640
+	clv
+	ldx #$00
+	rts
