067bfe7cc55caa3aacbf79a59ec8a4cec45e3b1d
Mikael Lund <Mlund@mac.com>
2023-08-15 20:23:09 +0200

    Fix test-memory freeze by clearing Z

--- src/debug.c
+++ src/debug.c
@@ -27,13 +27,15 @@ void debug_msg(char* msg)
     __asm__("LDA #$0a");
     __asm__("STA $D643");
     __asm__("CLV");
+    __asm__("LDZ #$0");
 #else
     asm volatile("lda #$0d\n"
                  "sta $d643\n"
                  "clv\n"
                  "lda #$0a\n"
                  "sta $d643\n"
-                 "clv\n" ::
+                 "clv\n"
+                 "ldz #0\n" :: // clearing Z seems to fix a xemu freeze. Why?
                      : "a");
 #endif
 }
